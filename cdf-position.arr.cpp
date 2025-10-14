74fc0b6ddf79218eb36758f533b62c9e91e17804
anik904-lang <hossainanik904@gmail.com>
2025-10-14 09:55:05 +0600

    Add files via upload

---
+++ cdf-position.arr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(arr[i]=10){
+            cout<<"A["<<i<<"]= "<<arr[i]<<"\n";
+        }
+
+    }
+
+}
